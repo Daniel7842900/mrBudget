3b9bbe797b69ab471bdf8ca35e2d3443869c3851
Daniel Lim <daniellim@Daniels-MacBook-Pro.local>
2022-02-05 12:44:06 -0800

    conditions added on dashboard createChart.js and index.ejs view

--- public/js/dashboard/createChart.js
+++ public/js/dashboard/createChart.js
@@ -1,63 +1,81 @@
 $(function () {
   // Month total variables
-  const monthTotal = $("#month-total")[0].getContext("2d");
-  const monthBudT = JSON.parse(monthBudTotal);
-  const monthExpT = JSON.parse(monthExpTotal);
-  const monthTotalData = [monthBudT, monthExpT];
-  const monthTotalLabels = ["Budget", "Expense"];
+  if ($("#month-total").length) {
+    const monthTotal = $("#month-total")[0].getContext("2d");
+    const monthBudT = JSON.parse(monthBudTotal);
+    const monthExpT = JSON.parse(monthExpTotal);
+    const monthTotalData = [monthBudT, monthExpT];
+    const monthTotalLabels = ["Budget", "Expense"];
 
-  // Month budget category variables
-  const monthBudCat = $("#month-budget-category")[0].getContext("2d");
-  const monthBudCatData = JSON.parse(monthBudCatJSON);
-  const mBudgetCatData = Object.values(monthBudCatData);
-  const mBudgetCatLables = Object.keys(monthBudCatData);
+    // Create a bar chart for monthly total
+    createChart(monthTotal, "bar", monthTotalData, monthTotalLabels);
+  }
 
-  // Month expense category variables
-  const monthExpCat = $("#month-expense-category")[0].getContext("2d");
-  const monthExpCatData = JSON.parse(monthExpCatJSON);
-  const mExpenseCatData = Object.values(monthExpCatData);
-  const mExpenseCatLables = Object.keys(monthExpCatData);
+  if ($("#month-budget-category").length) {
+    // Month budget category variables
+    const monthBudCat = $("#month-budget-category")[0].getContext("2d");
+    const monthBudCatData = JSON.parse(monthBudCatJSON);
+    const mBudgetCatData = Object.values(monthBudCatData);
+    const mBudgetCatLables = Object.keys(monthBudCatData);
 
-  // Week total variables
-  const weekTotal = $("#week-total")[0].getContext("2d");
-  const weekBudT = JSON.parse(weekBudTotal);
-  const weekExpT = JSON.parse(weekExpTotal);
-  const weekTotalData = [weekBudT, weekExpT];
-  const weekTotalLabels = ["Budget", "Expense"];
+    // Create a pie chart for monthly budget by category
+    createChart(monthBudCat, "pie", mBudgetCatData, mBudgetCatLables, "Budget");
+  }
 
-  // Week budget category variables
-  const weekBudCat = $("#week-budget-category")[0].getContext("2d");
-  const weekBudCatData = JSON.parse(weekBudCatJSON);
-  const wBudgetCatData = Object.values(weekBudCatData);
-  const wBudgetCatLables = Object.keys(weekBudCatData);
+  if ($("#month-expense-category").length) {
+    // Month expense category variables
+    const monthExpCat = $("#month-expense-category")[0].getContext("2d");
+    const monthExpCatData = JSON.parse(monthExpCatJSON);
+    const mExpenseCatData = Object.values(monthExpCatData);
+    const mExpenseCatLables = Object.keys(monthExpCatData);
 
-  // Week expense category variables
-  const weekExpCat = $("#week-expense-category")[0].getContext("2d");
-  const weekExpCatData = JSON.parse(weekExpCatJSON);
-  const wExpenseCatData = Object.values(weekExpCatData);
-  const wExpenseCatLables = Object.keys(weekExpCatData);
+    // Create a pie chart for monthly expense by category
+    createChart(
+      monthExpCat,
+      "pie",
+      mExpenseCatData,
+      mExpenseCatLables,
+      "Expense"
+    );
+  }
 
-  // Create a bar chart for monthly total
-  createChart(monthTotal, "bar", monthTotalData, monthTotalLabels);
+  if ($("#week-total").length) {
+    // Week total variables
+    const weekTotal = $("#week-total")[0].getContext("2d");
+    const weekBudT = JSON.parse(weekBudTotal);
+    const weekExpT = JSON.parse(weekExpTotal);
+    const weekTotalData = [weekBudT, weekExpT];
+    const weekTotalLabels = ["Budget", "Expense"];
 
-  // Create a pie chart for monthly budget by category
-  createChart(monthBudCat, "pie", mBudgetCatData, mBudgetCatLables, "Budget");
+    // Create a bar chart for weekly total
+    createChart(weekTotal, "bar", weekTotalData, weekTotalLabels);
+  }
 
-  // Create a pie chart for monthly expense by category
-  createChart(
-    monthExpCat,
-    "pie",
-    mExpenseCatData,
-    mExpenseCatLables,
-    "Expense"
-  );
+  if ($("#week-budget-category").length) {
+    // Week budget category variables
+    const weekBudCat = $("#week-budget-category")[0].getContext("2d");
+    const weekBudCatData = JSON.parse(weekBudCatJSON);
+    const wBudgetCatData = Object.values(weekBudCatData);
+    const wBudgetCatLables = Object.keys(weekBudCatData);
 
-  // Create a bar chart for weekly total
-  createChart(weekTotal, "bar", weekTotalData, weekTotalLabels);
+    // Create a pie chart for weekly budget by category
+    createChart(weekBudCat, "pie", wBudgetCatData, wBudgetCatLables, "Budget");
+  }
 
-  // Create a pie chart for weekly budget by category
-  createChart(weekBudCat, "pie", wBudgetCatData, wBudgetCatLables, "Budget");
+  if ($("#week-expense-category").length) {
+    // Week expense category variables
+    const weekExpCat = $("#week-expense-category")[0].getContext("2d");
+    const weekExpCatData = JSON.parse(weekExpCatJSON);
+    const wExpenseCatData = Object.values(weekExpCatData);
+    const wExpenseCatLables = Object.keys(weekExpCatData);
 
-  // Create a pie chart for weekly expense by category
-  createChart(weekExpCat, "pie", wExpenseCatData, wExpenseCatLables, "Expense");
+    // Create a pie chart for weekly expense by category
+    createChart(
+      weekExpCat,
+      "pie",
+      wExpenseCatData,
+      wExpenseCatLables,
+      "Expense"
+    );
+  }
 });
